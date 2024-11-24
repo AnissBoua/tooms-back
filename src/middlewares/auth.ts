466fefe1d7640ad52371aa9dc248ae0cebbb9b4d
Anisse <anissbouainbi@hotmail.it>
2024-11-24 17:58:27 +0100

    base auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import { JWT } from "@/services/jwt";
+
+const auth = (req: any, res: any, next: NextFunction) => {
+    const token = req.header('Authorization');
+    if (!token) {
+        return res.status(401).json({ error: 'Access denied. No token provided.' });
+    }
+    try {
+        const decoded = JWT.verify(token);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(400).json({ error: 'Invalid token.' });
+    }
+}
+
+export { auth };
