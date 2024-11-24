e6fd394fd038e6f67193cb990e1f2c0dfb503a9b
Anisse <anissbouainbi@hotmail.it>
2024-11-24 13:41:56 +0100

    avatar optional

--- src/models/user.ts
+++ src/models/user.ts
@@ -19,7 +19,7 @@ export class User {
     @Column({ type: 'varchar', length: 255 })
     password: string;
 
-    @Column({ type: 'varchar', length: 255 })
+    @Column({ type: 'varchar', length: 255, nullable: true })
     avatar: string;
 
     @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
