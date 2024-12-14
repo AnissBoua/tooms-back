50c1a6e3d9aa7e7a33bf864de96b7845d4f6d156
Anisse <anissbouainbi@hotmail.it>
2024-12-14 12:15:22 +0100

    more messages

--- src/routes/conversation.ts
+++ src/routes/conversation.ts
@@ -86,7 +86,7 @@ router.get('/:id/messages', [auth], async (req: any, res: Response) => {
         }
 
         const page = req.query.page ? parseInt(req.query.page as string) : 1;
-        const limit = 10;
+        const limit = 50;
         const offset = (page - 1) * limit;
         const messages = await MessageRepo.find({ where: { conversation: conversation }, relations: { user: true, conversation: true }, take: limit, skip: offset, order: { id: 'DESC' } });
 
