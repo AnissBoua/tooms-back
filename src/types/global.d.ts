05d2946a54eda0653e9f6bfda9d9f87e5d6816fa
Anisse <anissbouainbi@hotmail.it>
2024-12-29 14:55:57 +0100

    better types

--- src/types/global.d.ts
+++
@@ -1,9 +0,0 @@
-export {}; // This makes the file an external module
-
-declare global {
-    namespace Express {
-        interface Request {
-            user?: {sub: number}; // Add your User type here
-        }
-    }
-}
